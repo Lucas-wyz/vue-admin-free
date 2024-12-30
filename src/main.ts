9083dabe88eae9b2a6213fd27d309e38ed91b82b
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2024-12-30 22:12:04 +0800

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 
+import './assets/tailwind.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
