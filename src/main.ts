66622e41476cb863bca430b6a40cc6c47cd254e0
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-01-16 00:06:51 +0800

    add  devtools-api

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,74 @@ app.mount('#app')
 if (import.meta.env.MODE === 'development') {
   import('eruda').then((eruda) => eruda.default.init())
 }
+
+if (import.meta.env.MODE === 'development') {
+  // https://devtools.vuejs.org/
+  let { addCustomTab, addCustomCommand } = await import('@vue/devtools-api')
+  addCustomTab({
+    // unique identifier
+    name: 'eruda',
+    // title to display in the tab
+    title: 'eruda',
+    // any icon from material design icons or a URL to an image
+    icon: 'https://eruda.liriliri.io/favicon.ico',
+    // iframe view
+    view: {
+      type: 'iframe',
+      src: 'https://eruda.liriliri.io/zh/',
+    },
+    category: 'advanced', //  'pinned' | 'app' | 'modules' | 'advanced';
+  })
+
+  addCustomTab({
+    // unique identifier
+    name: 'element-plus',
+    // title to display in the tab
+    title: 'element-plus',
+    // any icon from material design icons or a URL to an image
+    icon: 'https://element-plus.org/images/element-plus-logo-small.svg',
+    // iframe view
+    view: {
+      type: 'iframe',
+      src: 'https://cn.element-plus.org/zh-CN/component/overview.html',
+    },
+    category: 'advanced', //  'pinned' | 'app' | 'modules' | 'advanced';
+  })
+
+  addCustomTab({
+    // unique identifier
+    name: 'vite',
+    // title to display in the tab
+    title: 'Vite',
+    // any icon from material design icons or a URL to an image
+    icon: 'https://cn.vite.dev/logo.svg',
+    // iframe view
+    view: {
+      type: 'iframe',
+      src: 'https://cn.vite.dev/guide/',
+    },
+    category: 'modules', //  'pinned' | 'app' | 'modules' | 'advanced';
+  })
+
+  // Add a custom command with submenu
+  addCustomCommand({
+    // unique identifier
+    id: 'Vue',
+    // title to display in the command
+    title: 'Vue',
+    // any icon from material design icons or a URL to an image
+    icon: 'https://cn.vuejs.org/logo.svg',
+    // submenu, which is shown when the menu is clicked
+    children: [
+      {
+        id: 'Vue:website',
+        title: 'Website',
+        icon: 'auto-awesome',
+        action: {
+          type: 'url',
+          src: 'https://cn.vuejs.org/',
+        },
+      },
+    ],
+  })
+}
