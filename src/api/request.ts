f2c844a45a1fc273cdffa6265b4cdff51ad3741d
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-01-15 17:37:16 +0800

    fix 更新axios配置

--- src/api/request.ts
+++ src/api/request.ts
@@ -10,41 +10,7 @@ const request = axios.create({
   //   baseURL: import.meta.env.VITE_API_BASE_URL,
   baseURL: '',
   timeout: 5000,
-  adapter: async (config: AxiosRequestConfig) => {
-    const { method = 'GET', headers = {}, data, url } = config
-    const fetchConfig: RequestInit = {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-        ...headers,
-      },
-      body: JSON.stringify(data),
-      signal,
-    }
-
-    try {
-      const response = await fetch(`${config.baseURL}${url}`, fetchConfig)
-      const res: ApiResponse = await response.json()
-      if (res.code !== 200) {
-        // 统一错误处理
-        throw new Error(res.message || '请求失败')
-      }
-      return {
-        data: res.data,
-        status: response.status,
-        statusText: response.statusText,
-        headers: response.headers,
-        config,
-        request: response,
-      }
-    } catch (error) {
-      if (error.name === 'AbortError') {
-        console.log('请求被取消')
-        return
-      }
-      throw error
-    }
-  },
+  adapter: 'fetch',
 })
 
 export { request, controller as source }
