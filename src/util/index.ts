324741eab6c587cc8ec8fa6a288ba7b53167e146
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-01-20 23:58:35 +0800

    add SHA1

--- src/util/index.ts
+++ src/util/index.ts
@@ -14,4 +14,26 @@ export const util = {
 
     return promise
   },
+  UUID(): string {
+    // 例如 "36b8f84d-df4e-4d49-b662-bcde71a8764f"
+    let crypto = new Crypto()
+    let uuid = crypto.randomUUID()
+    return uuid
+  },
+  async SHA256(text: string): Promise<string> {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(text)
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+    return hashHex
+  },
+  async SHA1(text: string): Promise<string> {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(text)
+    const hashBuffer = await crypto.subtle.digest('SHA-1', data)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+    return hashHex
+  },
 }
