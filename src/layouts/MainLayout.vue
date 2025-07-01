fcd04fa0356cb8ce79a7ed79ce224bfd82d7b1aa
Lucas-wyz <49265926+Lucas-wyz@users.noreply.github.com>
2025-07-01 23:59:54 +0800

    Fix Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,24 +1,31 @@
 <template>
   <el-container>
-    <el-aside width="200px" class="sidebar">
-      <el-menu class="sidebar-header h-50">
-        <h1>{{ title }}</h1>
-      </el-menu>
-      <el-menu router :default-active="currentRoute" :collapse="isCollapse">
-        <menu-recursive :menu-items="menus" />
-      </el-menu>
+    <el-aside>
+      <el-scrollbar height="600px">
+        <el-menu router :default-active="currentRoute" :collapse="isCollapse">
+          <el-menu-item>
+            <h1>{{ title }}</h1>
+          </el-menu-item>
+        </el-menu>
+
+        <el-menu router :default-active="currentRoute" :collapse="isCollapse">
+          <menu-recursive :menu-items="menus" />
+        </el-menu>
+      </el-scrollbar>
     </el-aside>
     <el-container>
-      <el-header>
+      <el-header class="h-30">
         <el-row class="w-full h-full">
           <el-col :span="20"> </el-col>
-          <el-col :span="4" class="h-full">
-            <el-icon-user-filled class="w-full h-full" style="" />
+          <el-col :span="4" class="h-full text-center">
+            <el-image style="" class="h-full rounded-full" :src="'user.png'" :fit="'fill'" />
           </el-col>
         </el-row>
       </el-header>
-      <el-main class="content p-5 overflow-y-auto">
-        <router-view></router-view>
+      <el-main class=" ">
+        <el-scrollbar height="600px">
+          <router-view></router-view>
+        </el-scrollbar>
       </el-main>
     </el-container>
   </el-container>
@@ -31,6 +38,8 @@ import MenuRecursive from '@/components/MenuRecursive.vue'
 import type { MenuItem } from '@/api'
 import { menuApi } from '@/api'
 
+import { Search, Plus, Edit, Delete, UserFilled } from '@element-plus/icons-vue'
+
 let title = import.meta.env.VITE_MENU_TITLE
 
 const route = useRoute()
@@ -38,7 +47,7 @@ const currentRoute = ref(route.path)
 const isCollapse = ref(false)
 
 // 模拟菜单数据
-const menus = ref<MenuItem[]>()
+const menus = ref<MenuItem[]>([])
 
 onMounted(async () => {
   let { data } = await menuApi.getMenuList()
